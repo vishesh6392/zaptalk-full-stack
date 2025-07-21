8bc718dabe5d5aefcf80eec7928647265576682a
vishesh6392 <152191613+vishesh6392@users.noreply.github.com>
2025-07-21 15:31:36 +0530

    Update socket.js
    socket frontend url

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // frontend URL
+    origin: "https://zaptalk-men.onrender.com", // frontend URL
     methods: ["GET", "POST"]
   }
 });
