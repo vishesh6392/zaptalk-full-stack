c47d4b0eea39afc91de94757f4a27cb4977eedaf
vishesh6392 <152191613+vishesh6392@users.noreply.github.com>
2025-07-21 15:30:49 +0530

    Update app.js
    update frontend url

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const port=process.env.PORT || 5000;
 
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://zaptalk-men.onrender.com"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
